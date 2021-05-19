83e669018715f2f00ceb7be7be686d7f9927cf3a
Herb Miller <herb@bobbingwide.com>
2021-05-19 08:25:03 +0100

    Respond to 'rendering' style do_action hooks to obtain template and template part information. Issue #2

--- sb-debug-block.php
+++ sb-debug-block.php
@@ -8,9 +8,9 @@
  * Author:            bobbingwide
  * License:           GPLv3
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       sb-starting-block
+ * Text Domain:       sb-debug-block
  *
- * @package           sb-starting-block
+ * @package           sb-debug-block
  */
 
 /**
@@ -27,28 +27,65 @@ function oik_sb_sb_debug_block_block_init() {
 
 function oik_sb_sb_debug_block_loaded() {
 	add_action( 'init', 'oik_sb_sb_debug_block_block_init' );
+	add_action( 'rendering_template_part', 'oik_sb_sb_debug_block_rendering_template_part', 10, 2);
+	add_action( 'rendering_template', 'oik_sb_sb_debug_block_rendering_template', 10, 3 );
 }
 /**
- * Implements post-edit block.
+ * Implements oik-sb/sb-debug-block block.
  *
- * If the user is authorised return a post edit link for the current post.
  *
- * @param $attrs
+ * @param $attributes
  * @param $content
- * @param $tag
+ * @param $block
  *
  * @return string
  */
-function oik_sb_sb_debug_block_dynamic_block( $attributes ) {
-	$classes = '';
-	if ( isset( $attributes['textAlign'] ) ) {
-		$classes .= 'has-text-align-' . $attributes['textAlign'];
-	}
-	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $classes ) );
-	$content = __( 'Debug block.', 'sb-starting-block');
-	$html = sprintf( '<div %1$s>%2$s</div>', $wrapper_attributes, $content );
-	// 	$html=\oik\oik_blocks\oik_blocks_check_server_func( "shortcodes/starting-block.php", "sb-starting-block", "oik_sb_sb_debug_block" );
+function oik_sb_sb_debug_block_dynamic_block( $attributes, $content, $block ) {
+	require_once __DIR__ . '/libs/class-oik-sb-sb-debug-block.php';
+	$sb_debug_block = new Oik_sb_sb_debug_block();
+	$html = $sb_debug_block->render( $attributes, $content, $block );
 	return $html;
 }
+
+/**
+ * Implements `rendering_template_part` action.
+ *
+ * If the class isn't loaded then load it
+ * When it's loaded get the instance
+ * Pass the action hook args to the rendering_template_part method.
+ *
+ * @param $attributes
+ * @param $seen_ids
+ */
+function oik_sb_sb_debug_block_rendering_template_part( $attributes, $seen_ids ) {
+	if ( !class_exists( 'Oik_sb_sb_debug_block_template_part' ) ) {
+		require_once __DIR__ . '/libs/class-oik-sb-sb-debug-block-template-part.php';
+	}
+	if ( class_exists( 'Oik_sb_sb_debug_block_template_part') ) {
+		$oik_sb_sdbtp = Oik_sb_sb_debug_block_template_part::get_instance();
+		$oik_sb_sdbtp->rendering_template_part( $attributes, $seen_ids );
+	}
+}
+
+/**
+ * Implements `rendering_template` action.
+ *
+ * If the class isn't loaded then load it
+ * When it's loaded get the instance
+ * Pass the action hook args to the rendering_template method.
+ *
+ * @param $attributes
+ * @param $seen_ids
+ */
+function oik_sb_sb_debug_block_rendering_template( $current_template, $type, $templates ) {
+	if ( !class_exists( 'Oik_sb_sb_debug_block_template' ) ) {
+		require_once __DIR__ . '/libs/class-oik-sb-sb-debug-block-template.php';
+	}
+	if ( class_exists( 'Oik_sb_sb_debug_block_template') ) {
+		$oik_sb_sdbt = Oik_sb_sb_debug_block_template::get_instance();
+		$oik_sb_sdbt->rendering_template( $current_template, $type, $templates );
+	}
+}
+
 
 oik_sb_sb_debug_block_loaded();
